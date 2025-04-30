e2bf6bdcadb360e378ae37e35eb4fcc90f6c96b4
navani004 <navaneethanovastrid@gmail.com>
2025-04-30 11:03:46 +0530

    navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import Contact from './component/Contact'
-import HomePage from './component/Home'
-import Login from './component/Login';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import HomePage from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Login from "./component/Login";
+import Register from "./component/Register";
 
-function App() {
+// const Home: React.FC = () => <h2>Home Page</h2>;
+// const About: React.FC = () => <h2>About Page</h2>;
+// const Contact: React.FC = () => <h2>Contact Page</h2>;
+// const Login: React.FC = () => <h2>Login Page</h2>;
+// const Register: React.FC = () => <h2>Register Page</h2>;
+
+const App: React.FC = () => {
   return (
-<><Login/>
-<HomePage/>
-</>
-   
+    <Router>
+      <Navbar />
+      <div style={{ padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
-    
-}
+};
 
 export default App;
