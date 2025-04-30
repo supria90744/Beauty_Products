cfeb11bbac2493f37e8f88a7cbc9a436d7f72878
Swathi <swathisenthil20@gmail.com>
2025-04-30 11:18:51 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Contact from './component/Contact'
+import About from './component/About'
 import HomePage from './component/Home'
 import Login from './component/Login';
 
@@ -6,6 +7,9 @@ function App() {
   return (
 <><Login/>
 <HomePage/>
+<Contact/>
+<About/>
+
 </>
    
   );
